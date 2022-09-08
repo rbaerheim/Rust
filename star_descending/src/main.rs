44713b56981e516c641d9c823484926311dddc7e
rbaerheim <rgbhm@hotmail.com>
2022-09-08 10:17:12 +0200

    Added skip every other row to star

--- star_descending/src/main.rs
+++ star_descending/src/main.rs
@@ -16,6 +16,20 @@ fn get_user_input() -> i32 {
     convert_user_string_to_int(user_input)
 }
 
+fn check_even() -> bool {
+    println!("Do you want every other star to be printed? y/n");
+    let mut even = String::new();
+    io::stdin().read_line(&mut even).unwrap_or_else(|e| {
+        println!("There was an error: {}", e);
+        process::exit(1)
+    });
+    match even.as_str().trim() {
+        "y" => true,
+        "n" => false,
+        _ => check_even(),
+    }
+}
+
 fn convert_user_string_to_int(user_input: String) -> i32 {
     match user_input.trim().parse::<i32>() {
         Ok(num) => num,
@@ -27,14 +41,19 @@ fn convert_user_string_to_int(user_input: String) -> i32 {
 }
 
 fn print_stars(num: i32) {
+    let mut even = 1;
+    if check_even() {
+        even = 2;
+    }
+
     if num > 0 {
-        for e in (0..num + 1).rev() {
+        for e in (0..num + 1).rev().step_by(even) {
             let stars = "*".repeat(e as usize);
             println!("{}", stars);
         }
     } else {
         let pos_num = num.abs();
-        for e in 0..pos_num + 1 {
+        for e in (0..pos_num + 1).step_by(even) {
             let stars = "*".repeat(e as usize);
             println!("{}", stars);
         }
