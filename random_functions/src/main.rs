aa0b6f3ea7fe50e6676a0a00540da7098219999b
rbaerheim <rgbhm@hotmail.com>
2022-09-10 18:07:32 +0200

    Yes

--- random_functions/src/main.rs
+++ random_functions/src/main.rs
@@ -1,4 +1,4 @@
-use loop_unwrap::unwrap_continue;
+use loop_unwrap::ToOption;
 use std::{io, num::ParseIntError, process};
 
 fn main() {
@@ -6,26 +6,51 @@ fn main() {
     happy_number();
 }
 
-#[allow(unused)]
-fn check_string_parse_to_signed_int(user_input: String) -> Result<i32, ParseIntError> {
-    match user_input.trim().parse::<i32>() {
+fn check_string_parse_to_signed_int(user_input: String) -> Result<u32, ParseIntError> {
+    match user_input.trim().parse::<u32>() {
         Ok(num) => Ok(num),
         Err(e) => Err(e),
     }
 }
 
-fn happy_number() -> bool {
+fn integers_to_digits(digit: u32) -> Vec<u32> {
+    let mut digit_copy: u32 = digit;
+    let mut digit_vector: Vec<u32> = Vec::with_capacity(10);
+    while digit_copy > 0 {
+        let num_to_vec: u32 = integer_squared(digit_copy % 10);
+        digit_copy = digit_copy / 10;
+        digit_vector.push(num_to_vec);
+    }
+    digit_vector
+}
+
+fn integer_squared(number: u32) -> u32 {
+    number.pow(2)
+}
+
+fn happy_number() -> u32 {
     println!(
         "Input a positive integer from 0 to 255 and I will tell you if it is a 'happy number': "
     );
-    loop {
+
+    let mut num = loop {
         let mut user_input = String::new();
 
         io::stdin().read_line(&mut user_input).unwrap_or_else(|e| {
             println!("There was an error: {}", e);
             process::exit(1)
         });
+        let num = loop_unwrap::unwrap_continue!(
+            check_string_parse_to_signed_int(user_input),
+            "Not a valid input, try again: "
+        );
+        break num;
+    };
 
-        let num = unwrap_continue!(check_string_parse_to_signed_int(user_input));
+    while num != 1 {
+        let digits_squared_vector_iter = integers_to_digits(num).iter();
+        num = digits_squared_vector_iter.sum()
     }
+
+    num
 }
