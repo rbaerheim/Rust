e37adfe9ef62ca5e1b13240b018b6e365b810fc8
rbaerheim <rgbhm@hotmail.com>
2022-09-13 11:12:25 +0200

    added happy number function to random functions

--- random_functions/src/main.rs
+++ random_functions/src/main.rs
@@ -1,56 +1,61 @@
 use loop_unwrap::ToOption;
-use std::{io, num::ParseIntError, process};
+use std::{collections::HashMap, io, num::ParseIntError, process};
 
 fn main() {
-    println!("Hello, world!");
-    happy_number();
+    let is_happy = happy_number();
+
+    println!("{}", is_happy);
 }
 
-fn check_string_parse_to_signed_int(user_input: String) -> Result<u32, ParseIntError> {
-    match user_input.trim().parse::<u32>() {
+fn check_string_parse_to_signed_int(user_input: &String) -> Result<u128, ParseIntError> {
+    match user_input.trim().parse::<u128>() {
         Ok(num) => Ok(num),
         Err(e) => Err(e),
     }
 }
 
-fn integers_to_digits(digit: u32) -> Vec<u32> {
-    let mut digit_copy: u32 = digit;
-    let mut digit_vector: Vec<u32> = Vec::with_capacity(10);
+fn integers_to_digits(digit: u128) -> Vec<u128> {
+    let mut digit_copy: u128 = digit;
+    let mut digit_hashmap: Vec<u128> = Vec::with_capacity(10);
     while digit_copy > 0 {
-        let num_to_vec: u32 = integer_squared(digit_copy % 10);
+        let num_to_vec: u128 = integer_squared(digit_copy % 10);
         digit_copy = digit_copy / 10;
-        digit_vector.push(num_to_vec);
+        digit_hashmap.push(num_to_vec);
     }
-    digit_vector
+    digit_hashmap
 }
 
-fn integer_squared(number: u32) -> u32 {
+fn integer_squared(number: u128) -> u128 {
     number.pow(2)
 }
 
-fn happy_number() -> u32 {
+fn happy_number() -> String {
     println!(
         "Input a positive integer from 0 to 255 and I will tell you if it is a 'happy number': "
     );
-
+    let mut user_input = String::new();
     let mut num = loop {
-        let mut user_input = String::new();
-
         io::stdin().read_line(&mut user_input).unwrap_or_else(|e| {
             println!("There was an error: {}", e);
             process::exit(1)
         });
         let num = loop_unwrap::unwrap_continue!(
-            check_string_parse_to_signed_int(user_input),
+            check_string_parse_to_signed_int(&user_input),
             "Not a valid input, try again: "
         );
         break num;
     };
 
-    while num != 1 {
-        let digits_squared_vector_iter = integers_to_digits(num).iter();
-        num = digits_squared_vector_iter.sum()
-    }
+    let mut control_hashmap = HashMap::new();
 
-    num
+    loop {
+        num = integers_to_digits(num).iter().sum();
+        if num == 1 {
+            return format!("{} is a happy number!", user_input.trim());
+        }
+        if control_hashmap.contains_key(&num) {
+            return format!("{} is not a happy number..", user_input.trim());
+        }
+        control_hashmap.insert(num, false);
+    }
 }
